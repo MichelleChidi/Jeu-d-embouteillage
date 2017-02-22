74d9874c89a11198a760f1b7076540ed0b3a3976
Aerr0 <alexis.jaunet@laposte.net>
2017-02-22 17:18:41 +0100

    Créateur de carte
    Ces fichiers incluts deux objets Draggable qui me servent à mon drag'n drop. Je pourrais vous les expliquer plus en détails.
    
    Sinon les autres fichiers sont juste des JFrame et JPanel.
    Pour tester, lancer CreateCard.

---
+++ CreateCard.java
@@ -0,0 +1,203 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import model.DraggableImageComponent;
+
+public class CreateCard {
+
+    public static JFrame frame;
+    public static PanelCreateCard createBoard;
+    private JButton sauvegarder;
+	private JButton effacer;
+	private JButton retour;
+    
+    private static Image redCar;
+    private static int colorHorizontalTruck;
+	private static int colorVerticalTruck;
+	private static int colorHorizontalCar;
+	private static int colorVerticalCar;
+	private static ArrayList<Image> verticalTrucks;
+	private static ArrayList<Image> horizontalTrucks;
+	private static ArrayList<Image> verticalCars;
+	private static ArrayList<Image> horizontalCars;
+	
+	public CreateCard() {
+		initGraphic();
+		createModel();
+		createView();
+		placeComponent();
+		createController();
+	}
+	
+	public void display() {
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
+	private void initGraphic() {
+		// création de la voiture rouge
+	    redCar = Toolkit.getDefaultToolkit().createImage("images/redCar.png");
+	    
+        // création des ArrayList<Image>
+	    verticalTrucks   = new ArrayList<Image>();
+	    horizontalTrucks = new ArrayList<Image>();
+	    verticalCars     = new ArrayList<Image>();
+	    horizontalCars   = new ArrayList<Image>();
+	    
+	    // verticalTrucks 
+	    verticalTrucks.add(Toolkit.getDefaultToolkit().createImage("images/yellowTruckVertical.png"));
+	    verticalTrucks.add(Toolkit.getDefaultToolkit().createImage("images/blueTruckVertical.png"));
+	    verticalTrucks.add(Toolkit.getDefaultToolkit().createImage("images/purpleTruckVertical.png"));
+	    
+	    // horizontalTruks
+	    horizontalTrucks.add(Toolkit.getDefaultToolkit().createImage("images/yellowTruckHorizontal.png"));
+	    horizontalTrucks.add(Toolkit.getDefaultToolkit().createImage("images/blueTruckHorizontal.png"));
+	    horizontalTrucks.add(Toolkit.getDefaultToolkit().createImage("images/purpleTruckHorizontal.png"));
+	    
+	    // verticalCars
+	    verticalCars.add(Toolkit.getDefaultToolkit().createImage("images/orangeCarVertical.png"));
+	    verticalCars.add(Toolkit.getDefaultToolkit().createImage("images/blueCarVertical.png"));
+	    verticalCars.add(Toolkit.getDefaultToolkit().createImage("images/greenCarVertical.png"));
+	    
+	    // horizontalCars
+	    horizontalCars.add(Toolkit.getDefaultToolkit().createImage("images/orangeCarHorizontal.png"));
+	    horizontalCars.add(Toolkit.getDefaultToolkit().createImage("images/blueCarHorizontal.png"));
+	    horizontalCars.add(Toolkit.getDefaultToolkit().createImage("images/greenCarHorizontal.png"));
+        
+        colorHorizontalTruck = 1;
+	    colorVerticalTruck   = 3;
+	    colorHorizontalCar   = 1;
+	    colorVerticalCar     = 3;
+	    
+	    java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                loadVehicles();
+            }
+        });
+	}
+	
+	public void createModel() {
+		
+	}
+	
+	public void createView() {
+		final int height = 720;
+		final int width  = 1280;
+		
+		frame = new JFrame("Créateur de carte");
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setLayout(new BorderLayout());
+		
+        createBoard = new PanelCreateCard();
+        createBoard.setLayout(null);
+        createBoard.setPreferredSize(new Dimension(width, height-100));
+        
+        sauvegarder = new JButton("Sauvegarder");
+		sauvegarder.setPreferredSize(new Dimension(200, 50));
+		
+		effacer = new JButton("Effacer");
+		effacer.setPreferredSize(new Dimension(200, 50));
+		
+		retour = new JButton("Retour");
+		retour.setPreferredSize(new Dimension(200, 50));
+	}
+	
+	public void placeComponent() {
+		frame.add(createBoard, BorderLayout.CENTER);
+		
+		JPanel p = new JPanel(new FlowLayout(FlowLayout.CENTER)); {
+			p.add(sauvegarder);
+			p.add(effacer);
+			p.add(retour);
+		}
+		frame.add(p, BorderLayout.SOUTH);
+	}
+	
+	public void createController() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+    public static void loadVehicles() {
+        createBoard.removeAll();
+       
+        addNewVehicle(redCar, 310, 224, 139, 67);
+        addNewVehicle(getHorizontalTruck(), 20, 20, 207, 66);
+        addNewVehicle(getVerticalTruck(), 90, 110, 66, 207);
+        addNewVehicle(getHorizontalCar(), 50, 345, 139, 67);
+        addNewVehicle(getVerticalCar(), 90, 440, 67, 139);
+        
+        createBoard.repaint();
+    }
+
+    public static void addNewVehicle(Image img, int posX, int posY, int width, int height) {
+        //Creates a draggableImageComponent and adds loaded image
+        DraggableImageComponent comp = new DraggableImageComponent();
+        createBoard.add(comp);//Adds this component to main container
+        comp.setImage(img);//Sets image
+        comp.setOverbearing(true);//On click ,this panel gains lowest z-buffer
+
+        comp.setSize(width, height);
+        comp.setLocation(posX, posY);
+        
+        createBoard.repaint();
+    }
+
+    public static int getRandom(int range) {
+        int r = (int) (Math.random() * range) - range;
+        return r;
+    }
+    
+    // OUTILS GRAPHIC
+	private static Image getVerticalTruck() {
+		int nb = colorVerticalTruck%3;
+		colorVerticalTruck ++;
+		
+		return verticalTrucks.get(nb);
+	}
+	
+	private static Image getHorizontalTruck() {
+		int nb = colorHorizontalTruck%3;
+		colorHorizontalTruck ++;
+		
+		return horizontalTrucks.get(nb);
+	}
+	
+	private static Image getVerticalCar() {
+		int nb = colorVerticalCar%3;
+		colorVerticalCar ++;
+		
+		return verticalCars.get(nb);
+	}
+	
+	private static Image getHorizontalCar() {
+		int nb = colorHorizontalCar%3;
+		colorHorizontalCar ++;
+		
+		return horizontalCars.get(nb);
+	}
+	
+	// MAIN
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new CreateCard().display();
+            }
+        });
+	}
+}
+
+
---
+++ PanelCreateCard.java
@@ -0,0 +1,67 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class PanelCreateCard extends JPanel {
+
+	public PanelCreateCard() {
+		final int height = 720;
+		final int width  = 1280;
+		
+		setLayout(null);
+		setPreferredSize(new Dimension(width, height-100));
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		// board
+		Image image = null;
+		try {
+			image = ImageIO.read(new File("images/board.png"));
+		} catch (IOException e) { }
+		
+	    g.drawImage(image, 260, 10, this);
+	    
+	    // carrés de gauche
+	    // camion horizontal
+	    g.setColor(Color.WHITE);
+	    g.fillRect(10, 10, 225, 85);	
+	    g.setColor(Color.BLACK);
+	    g.drawRect(10, 10, 225, 85);
+	    
+	    // camion vertical
+	    g.setColor(Color.WHITE);
+	    g.fillRect(80, 100, 85, 225);
+	    g.setColor(Color.BLACK);
+	    g.drawRect(80, 100, 85, 225);
+	    
+	    // voiture horizontal
+	    g.setColor(Color.WHITE);
+	    g.fillRect(40, 335, 160, 85);
+	    g.setColor(Color.BLACK);
+	    g.drawRect(40, 335, 160, 85);
+	    
+	    // voiture vertical
+	    g.setColor(Color.WHITE);
+	    g.fillRect(80, 430, 85, 160);
+	    g.setColor(Color.BLACK);
+	    g.drawRect(80, 430, 85, 160);
+	    
+	    // ligne séparatrice
+	    g.setColor(Color.BLACK);
+	    g.drawLine(245, 10, 245, 590);
+
+	    g.finalize();
+	}
+}
