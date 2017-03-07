915492831d85e98ffcb4333943affb6acf112461
MichelleChidi <michelle.njemanze@yahoo.com>
2017-03-07 17:20:46 +0100

    Create State.java

---
+++ cmd/State.java
@@ -0,0 +1,13 @@
+package cmd;
+
+public enum State {
+    /**
+     * Caractérise l'état "faire" pour une commande.
+     */
+    DO,
+    
+    /**
+     * Caractérise l'état "défaire" pour une commande.
+     */
+    UNDO
+}
